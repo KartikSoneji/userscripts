48340f8cb77362ee6450c81bedc9ca82b8af0f88
Kartik Soneji <kartiksoneji@rocketmail.com>
2021-07-16 18:36:54 +0530

    Improve performance of YouTube Favicon script.

--- YouTube Favicon to Channel Logo.js
+++ YouTube Favicon to Channel Logo.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			YouTube Favicon to Channel logo
-// @version			1.2
+// @version			1.3
 // @description		Changes the YouTube Favicon to the Channel Logo
 // @author			Kartik Soneji
 // @icon			https://www.youtube.com/s/desktop/90b31458/img/favicon_144x144.png
@@ -11,29 +11,27 @@
 // @exclude			https://www.youtube.com/embed/*
 // @exclude			https://www.youtube.com/live_chat*
 // @grant			none
-// @run-at			document-end
+// @run-at			document-start
 // @noframes
 // @supportURL		https://gitlab.com/KartikSoneji/userscripts/issues
 // @updateURL		https://openuserjs.org/meta/KartikSoneji/YouTube_Favicon_to_Channel_logo.meta.js
 // @downloadURL		https://openuserjs.org/install/KartikSoneji/YouTube_Favicon_to_Channel_logo.user.js
 // ==/UserScript==
 
-
-(function(){
-	'use strict';
-
-	const urlRegex = /https:\/\/yt3.ggpht.com\/a[/-]{1,2}[a-zA-Z\d-_]+/;
-
-	console.log("Started");
-
-	function run(){
-		getChannelLogo(window.location.href).then(setRoundFavicon);
-	}
-
+(() => {
 	window.addEventListener("load", () => {
-		setRoundFavicon(JSON.stringify(window.ytInitialData).match(urlRegex)[0]);
+		let { thumbnails } =
+			window.ytInitialData.header?.c4TabbedHeaderRenderer?.avatar ??
+			window.ytInitialData.contents?.twoColumnWatchNextResults?.results.results
+				.contents.find(e => "videoSecondaryInfoRenderer" in e)
+				.videoSecondaryInfoRenderer.owner.videoOwnerRenderer.thumbnail;
+		setRoundFavicon(thumbnails[thumbnails.length - 1]?.url);
 
-		let observer = new MutationObserver(e => setTimeout(run, 150));
+		let observer = new MutationObserver(() =>
+			setTimeout(() => {
+				setRoundFavicon(document.querySelector("#meta-contents #avatar img[src]")?.src);
+			}, 100)
+		);
 		observer.observe(document.querySelector("title"), {
 			childList: true,
 			attributes: true,
@@ -41,30 +39,17 @@
 		});
 	});
 
-	async function getChannelLogo(url){
-		return fetch(url)
-			.then(e => e.text())
-			.then(e => {
-				let u = e.match(urlRegex)[0];
-				console.log("Favicon:", u);
-				return u;
-			});
-	}
-
 	function setRoundFavicon(dataUrl){
 		roundImageToDataUrl(dataUrl).then(setFavicon);
 	}
 
 	async function roundImageToDataUrl(url){
 		return new Promise((resolve, reject) => {
-			if(url.src)
-				url = url.src;
-
 			let img = new Image();
 			img.crossOrigin = "anonymous";
-			img.src = url;
+			img.src = url.src ?? url;
 
-			img.onload = function(){
+			img.onload = () => {
 				let canvas = document.createElement("canvas"),
 					g = canvas.getContext("2d");
 
@@ -72,13 +57,19 @@
 				canvas.height = img.naturalHeight;
 
 				g.beginPath();
-				g.arc(canvas.width/2, canvas.height/2, canvas.width/2, 0, Math.PI * 2);
+				g.arc(
+					canvas.width/2,
+					canvas.height/2,
+					canvas.width/2,
+					0,
+					Math.PI * 2
+				);
 				g.clip();
 
 				g.drawImage(img, 0, 0);
 
 				resolve(canvas.toDataURL());
-			}
+			};
 		});
 	}
 
